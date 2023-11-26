faf7ff58225d858cc8fb30eaf01676c1bab74fa4
Sebastian Goiburo <92232484+SebaGoiburo@users.noreply.github.com>
2023-11-26 15:55:57 -0300

    Inclui el raw del archivo Json

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ function mostrarMensajeBienvenida(username) {
 //mucho más prolijo y modularizado supongo.
 
 
-fetch('/js/productos.json')
+fetch('https://raw.githubusercontent.com/SebaGoiburo/PFGoiburo/main/js/productos.json')
 .then( respuesta => respuesta.json())
 .then( data  =>{
     console.log(data);
